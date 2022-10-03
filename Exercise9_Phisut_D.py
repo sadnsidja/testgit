3fc258b00ccc6dabfeeea348904e5c89363da9ee
sadnsidja <113336853+sadnsidja@users.noreply.github.com>
2022-10-03 14:30:54 +0700

    Create Exercise9_Phisut_D.py

---
+++ Exercise9_Phisut_D.py
@@ -0,0 +1,6 @@
+username = input("Name: ")
+password = input("PassWord: ")
+while username != "HelloWorld" or password != "7777":
+    username = input("Name: ")
+    password = input("PassWord: ")
+print("Done")
