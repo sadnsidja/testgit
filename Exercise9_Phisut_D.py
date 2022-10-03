cc18cb1cffb45d7b26c2b691fedc961a00214d99
sadnsidja <113336853+sadnsidja@users.noreply.github.com>
2022-10-03 14:33:50 +0700

    Update Exercise9_Phisut_D.py

--- Exercise9_Phisut_D.py
+++ Exercise9_Phisut_D.py
@@ -1,6 +1,7 @@
 username = input("Name: ")
 password = input("PassWord: ")
 while username != "HelloWorld" or password != "7777":
+    print("pls try again")
     username = input("Name: ")
     password = input("PassWord: ")
-print("Done")
+print("welcome back",username)
